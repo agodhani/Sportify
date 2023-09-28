e01165973dcd6bcd0af1741fbf42c06f153eb137
agodhani <agodhani2021@gmail.com>
2023-09-28 18:20:19 -0400

    Initial Commit

---
+++ 307_Sportify/_07_SportifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  _07_SportifyApp.swift
+//  307_Sportify
+//
+//  Created by Akshay Godhani on 9/28/23.
+//
+
+import SwiftUI
+
+@main
+struct _07_SportifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
