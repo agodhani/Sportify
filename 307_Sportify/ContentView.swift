b5b5cb8f5a5ba9f6c23333f2b7e1b4b3d1b491ed
agodhani <agodhani2021@gmail.com>
2023-09-28 20:41:14 -0400

    dev branch creation

--- 307_Sportify/ContentView.swift
+++ 307_Sportify/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("GithubTest")
+            Text("This is the dev branch")
         }
         .padding()
     }
