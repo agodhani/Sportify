8941193ce09af97eef5e62bdb993040b90124570
agodhani <agodhani2021@gmail.com>
2023-09-29 00:10:25 -0400

    LogSignUp Initalization Page, and new LogInView created

---
+++ 307_Sportify/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LogInView.swift
+//  307_Sportify
+//
+//  Created by Akshay Godhani on 9/29/23.
+//
+
+import SwiftUI
+
+struct LogInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogInView()
+}
