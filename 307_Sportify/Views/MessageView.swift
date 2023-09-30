e95f3a3cd39f2a0e126d62bb4dddf259928ac0cb
realBrando <brandonandrew0@gmail.com>
2023-09-30 15:12:36 -0400

    Tab Pages

---
+++ 307_Sportify/Views/MessageView.swift
@@ -0,0 +1,24 @@
+//
+//  MessageView.swift
+//  307_Sportify
+//
+//  Created by Andrew Brandon on 9/30/23.
+//
+
+import SwiftUI
+
+struct MessageView: View {
+    var body: some View {
+        ZStack {
+            Color.black
+                    
+            Image(systemName: "plus.message.fill")
+                .foregroundColor(.blue)
+                .font(.system(size: 100.0))
+        }
+    }
+}
+
+#Preview {
+    MessageView()
+}
