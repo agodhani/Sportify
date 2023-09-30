e80d57d52b57c04ca323f170cadd24430dda113f
agodhani <agodhani2021@gmail.com>
2023-09-30 13:57:35 -0400

    merge conflict

---
+++ 307_Sportify/Views/ForgotPasswordView.swift
@@ -0,0 +1,18 @@
+//
+//  ForgotPasswordView.swift
+//  307_Sportify
+//
+//  Created by Joshua Tseng on 9/29/23.
+//
+
+import SwiftUI
+
+struct ForgotPasswordView: View {
+    var body: some View {
+        Text("test text")
+    }
+}
+
+#Preview {
+    ForgotPasswordView()
+}
