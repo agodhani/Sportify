e5bddd81ae3a9632c216edaf159ae77689d9bc2b
Alexcmoraes <alexandre.c.moraes@hotmail.com>
2023-09-30 22:00:46 -0400

    Event struct and methods

---
+++ 307_Sportify/Objects/Event.swift
@@ -0,0 +1,82 @@
+//
+//  Event.swift
+//  307_Sportify
+//
+//  Created by Alexandre Cunha Moraes on 9/30/23.
+//
+
+import Foundation
+import CoreLocation
+
+struct Event: Identifiable {
+    internal let id: String
+    var sprtsList = ["Tennis", "Table Tennis", "Volleyball", "Soccer", "Basketball", "Football", "Baseball", "Badminton", "Golf", "Cycling", "Running", "Hockey", "Spikeball", "Handball", "Lacrosse", "Squash"]
+    var sport: Int
+    var date: Date
+    var location: CLLocation
+    var numAttendees: Int
+    var attendeeList: Set<User>
+    var privateEvent: Bool
+    var maxParticipants: Int
+    var adminsList: Set<User>
+    var eventHost: User
+    private var code: String
+    var blackList: Set<User>
+    
+    mutating func kickAttendee(attendee: User) {
+        self.attendeeList.remove(attendee)
+        self.blackList.insert(attendee)
+    }
+    
+    mutating func setSport(sport: Int) {
+        self.sport = sport
+    }
+    
+    mutating func setDate(date: Date) {
+        self.date = date
+    }
+    
+    mutating func setLocation(location: CLLocation) {
+        self.location = location
+    }
+    
+    mutating func setNumAttendees(num: Int) {
+        self.numAttendees = num
+    }
+    
+    mutating func addAttendee(attendee: User) {
+        self.attendeeList.insert(attendee)
+    }
+    
+    mutating func setMaxParticipants(num: Int) {
+        self.maxParticipants = num
+    }
+    
+    mutating func setAdminList() {
+        
+    }
+    
+    mutating func addAdmin(admin: User) {
+        self.adminsList.insert(admin)
+    }
+    
+    mutating func setHost(host: User) {
+        self.eventHost = host
+    }
+    
+    func deleteEvent() {
+        
+    }
+    
+    func getDate() -> Date {
+        return self.date
+    }
+    
+    func getLocation() -> CLLocation{
+        return self.location
+    }
+    
+    func getNumAttendees() -> Int{
+        return numAttendees
+    }
+}
