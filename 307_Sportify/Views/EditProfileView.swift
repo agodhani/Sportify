6833fbc0c2aec40406b8b892da360192b06cf2d7
realBrando <brandonandrew0@gmail.com>
2023-10-04 22:39:58 -0400

    Created profile view and edit profile view with button linking the two views

---
+++ 307_Sportify/Views/EditProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfileView.swift
+//  307_Sportify
+//
+//  Created by Andrew Brandon on 10/4/23.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditProfileView()
+}
--- 307_Sportify/Views/ProfileView.swift
+++ 307_Sportify/Views/ProfileView.swift
@@ -4,17 +4,136 @@
 //
 //  Created by Andrew Brandon on 9/30/23.
 //
+//This is just a rough draft, I realized later that I could
+//  set locations of things, but for now just created
+//  spaces with Hstack, but will change in future 10/4
 
 import SwiftUI
+import Firebase
 
 struct ProfileView: View {
     var body: some View {
         ZStack {
             Color.black
+            VStack {
+                HStack {
+                    Spacer()
+                    Image("DefaultProfile")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 200, height: 200)
+                        .cornerRadius(200)
+                        .clipShape(Circle())
+                        .padding()
+                    Spacer()
+                }
+                HStack{
+                    Text("Name:")
+                        .foregroundColor(Color("SportGold"))
+                    Text("Andrew Brandon")
+                        .foregroundColor(Color("SportGold"))
                     
-            Image(systemName: "person")
-                .foregroundColor(.gray)
-                .font(.system(size: 100.0))
+                    Spacer()
+                }
+                HStack{
+                    Spacer()
+                }
+                HStack{
+                    Spacer()
+                }
+                HStack{
+                    Spacer()
+                }
+                HStack{
+                    Spacer()
+                }
+                HStack{
+                    Spacer()
+                }
+                HStack{
+                    Text("Username:")
+                        .foregroundColor(Color("SportGold"))
+                    Text("brando_sports")
+                        .foregroundColor(Color("SportGold"))
+                    Spacer()
+                }
+                HStack{
+                    Spacer()
+                }
+                HStack{
+                    Spacer()
+                }
+                HStack{
+                    Spacer()
+                }
+                HStack{
+                    Spacer()
+                }
+                HStack{
+                    Spacer()
+                }
+                HStack{
+                    Text("Location:")
+                        .foregroundColor(Color("SportGold"))
+                    Text("47906")
+                        .foregroundColor(Color("SportGold"))
+                    Spacer()
+                }
+                
+                HStack{
+                    Spacer()
+                }
+                HStack{
+                    Spacer()
+                }
+                HStack{
+                    Spacer()
+                }
+                HStack{
+                    Spacer()
+                }
+                HStack{
+                    Spacer()
+                }
+                HStack{
+                    Text("Sport Preferences:")
+                        .foregroundColor(Color("SportGold"))
+                    Text("Football, Dodgeball, Basketball")
+                        .foregroundColor(Color("SportGold"))
+                    Spacer()
+                }
+                HStack{
+                    Spacer()
+                }
+                HStack{
+                    Spacer()
+                }
+                HStack{
+                    Spacer()
+                }
+                HStack{
+                    Spacer()
+                }
+                HStack{
+                    Spacer()
+                }
+                HStack{
+                    Spacer()
+                }
+            
+                NavigationLink(destination:EditProfileView()){
+                    Button("Edit Profile"){
+                        
+                    }
+                    .foregroundColor(.black)
+                    .fontWeight(.heavy)
+                    .frame(width: 225, height: 50)
+                    .background(Color("SportGold"))
+                    .cornerRadius(200)
+                }
+                Spacer()
+                
+            }
         }
     }
 }
