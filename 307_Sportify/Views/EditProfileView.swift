34518d15f676e0ae4bc15bb00acc7405333a4e70
Alexcmoraes <alexandre.c.moraes@hotmail.com>
2023-10-08 22:19:34 -0400

    Tried to implement view controller for edit profile

--- 307_Sportify/Views/EditProfileView.swift
+++ 307_Sportify/Views/EditProfileView.swift
@@ -7,7 +7,65 @@
 
 import SwiftUI
 
+class EditProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, ObservableObject {
+    @EnvironmentObject var userAuth: UserAuthentication
+    
+    let profileImage: UIImageView = {
+        let profileImage = UIImageView()
+        profileImage.image = UIImage(named:"DefaultProfile")
+        profileImage.contentMode = .scaleAspectFit
+        return profileImage
+    }()
+    
+    func presentPhotoActionSheet() {
+        let actionSheet = UIAlertController(title: "Profile Picture",
+                                            message: "How would you like to select a picture?",
+                                            preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: "Choose from camera Roll",
+                                            style: .default,
+                                            handler: { [weak self] _ in
+                                            self?.presentPhotoPicker()
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Take picture",
+                                            style: .default,
+                                            handler: { [weak self] _ in
+                                            self?.presentCamera()
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Cancel",
+                                            style: .cancel,
+                                            handler: nil))
+        
+        present(actionSheet, animated: true)
+    }
+    
+    func presentCamera() {
+        let vc = UIImagePickerController()
+        vc.sourceType = .camera
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
+    }
+    
+    func presentPhotoPicker() {
+        let vc = UIImagePickerController()
+        vc.sourceType = .photoLibrary
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+}
+
 struct EditProfileView: View {
+    @StateObject var controller = EditProfileViewController()
     @EnvironmentObject var userAuth: UserAuthentication
     @State private var newUsername = ""
     @State private var newPassword = ""
@@ -22,14 +80,14 @@ struct EditProfileView: View {
                     Button(action: {
                        print("pic clicked")
                     }) {
-                      //  if (userAuth.currUser?.getProfilePic() == nil) {
-                            Image("DefaultProfile")
+                 //       if (userAuth.currUser?.getProfilePic() == nil) {
+                            Image(uiImage: controller.profileImage.image!)
                                 .resizable()
                                 .aspectRatio(contentMode: .fill)
                                 .frame(width: 200, height: 200)
                                 .cornerRadius(200)
                                 .clipShape(Circle())
-//                       } else {
+//                        } else {
 //                            Image(uiImage: (userAuth.currUser?.getProfilePic())!)
 //                                .resizable()
 //                                .aspectRatio(contentMode: .fill)
