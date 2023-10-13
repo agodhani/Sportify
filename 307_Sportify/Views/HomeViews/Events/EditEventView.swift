d93e5073372a073a661856638ac3d8364658f4b7
Alexcmoraes <alexandre.c.moraes@hotmail.com>
2023-10-13 04:30:41 -0400

    Very good UI for edit event

--- 307_Sportify/Views/HomeViews/Events/EditEventView.swift
+++ 307_Sportify/Views/HomeViews/Events/EditEventView.swift
@@ -13,77 +13,75 @@ struct EditEventView: View {
     @State var newDate: Date = Date()
     @State var newLocation: String = ""
     @State var newNumPeople: Int = 0
+    @State var newPrivate: Bool = false
     
     var body: some View {
-   //     @State var eventName = event.eventName
- //       @State var eventDate = event.date.formatted()
-  //      let eventArr = eventDate.split(separator: ",", maxSplits: 2, omittingEmptySubsequences: true)
- //       @State var dateStr = String(eventArr[0])
-//        @State var timeStr = String(eventArr[1])
         @State var eventLocation = "1234 Temp Location, Lafayette, IN 94507"
         ZStack {
             Color.black.ignoresSafeArea()
             
             VStack(alignment: .trailing) {
-                Toggle("Private Event")
-                .foregroundColor(event.getPrivColor())
-                .font(.system(size: 12, weight: .heavy, design: .default))
-                .multilineTextAlignment(.leading)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.leading, 20)
+//                Toggle("Private Event", isOn: $newPrivate)
+//                .foregroundColor(Color("SportGold"))
+//                .font(.system(size: 12, weight: .heavy, design: .default))
+//                .multilineTextAlignment(.leading)
+//                .padding(.leading, 20)
+//                
+//                TextField("New event name", text: <#T##Binding<String>#>)
+//                    .foregroundColor(.white)
+//                    .font(.system(size: 40, weight: .heavy, design: .default))
+//                    .multilineTextAlignment(.leading)
+//                    .frame(maxWidth: .infinity, alignment: .leading)
+//                    .padding(.leading, 20)
+//                    .padding(.top, -15)
+//                
+//                HStack {
+//                    TextField(dateStr, text: $dateStr) // TODO need to be able to update this
+//                        .foregroundColor(.white)
+//                        .font(.system(size: 20, weight: .heavy, design: .default))
+//                        .multilineTextAlignment(.leading)
+//                        .frame(maxWidth: .infinity, alignment: .leading)
+//                        .padding(.leading, 20)
+//                    
+//                    Text("at")
+//                        .foregroundColor(.white)
+//                        .font(.system(size: 20, weight: .heavy, design: .default))
+//                        //.frame(maxWidth: .infinity, alignment: .leading)
+//                        .padding(.leading, -55)
+//                    
+//                    TextField(timeStr, text: $timeStr) // need to be able to update this
+//                        .foregroundColor(.white)
+//                        .font(.system(size: 20, weight: .heavy, design: .default))
+//                        .multilineTextAlignment(.leading)
+//                        //.frame(maxWidth: .infinity, alignment: .leading)
+//                        .padding(.leading, -40)
+//                }
+//                
+//
+//                
+//                // Address / location
+//                // TODO
+//                TextField(eventLocation, text: $eventLocation) // need to be able to update this
+//                .foregroundColor(.gray)
+//                .font(.system(size: 15, weight: .heavy, design: .default))
+//                .multilineTextAlignment(.leading)
+//                .frame(maxWidth: .infinity, alignment: .leading)
+//                .padding(.leading, 20)
+//                
+//                Rectangle()
+//                    .fill(Color.white)
+//                    .frame(width: 391, height: 2)
+//                    .padding(1)
+//                
+//                TextField(event.getDescription(), text: $event.description)
+//                    .foregroundColor(.gray)
+//                    .font(.system(size: 15, weight: .heavy, design: .default))
+//                    .multilineTextAlignment(.leading)
+//                    .frame(maxWidth: .infinity, alignment: .leading)
+//                    .padding(.leading, 20)
                 
-                TextField(event.eventName, text: $event.eventName)
-                    .foregroundColor(.white)
-                    .font(.system(size: 40, weight: .heavy, design: .default))
-                    .multilineTextAlignment(.leading)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.leading, 20)
-                    .padding(.top, -15)
-                
-                HStack {
-                    TextField(dateStr, text: $dateStr) // TODO need to be able to update this
-                        .foregroundColor(.white)
-                        .font(.system(size: 20, weight: .heavy, design: .default))
-                        .multilineTextAlignment(.leading)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.leading, 20)
-                    
-                    Text("at")
-                        .foregroundColor(.white)
-                        .font(.system(size: 20, weight: .heavy, design: .default))
-                        //.frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.leading, -55)
-                    
-                    TextField(timeStr, text: $timeStr) // need to be able to update this
-                        .foregroundColor(.white)
-                        .font(.system(size: 20, weight: .heavy, design: .default))
-                        .multilineTextAlignment(.leading)
-                        //.frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.leading, -40)
-                }
-                
-
-                
-                // Address / location
-                // TODO
-                TextField(eventLocation, text: $eventLocation) // need to be able to update this
-                .foregroundColor(.gray)
-                .font(.system(size: 15, weight: .heavy, design: .default))
-                .multilineTextAlignment(.leading)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.leading, 20)
-                
-                Rectangle()
-                    .fill(Color.white)
-                    .frame(width: 391, height: 2)
-                    .padding(1)
-                
-                TextField(event.getDescription(), text: $event.description)
-                    .foregroundColor(.gray)
-                    .font(.system(size: 15, weight: .heavy, design: .default))
-                    .multilineTextAlignment(.leading)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.leading, 20)
+                Toggle("Private Event", isOn: $newPrivate)
+                    .foregroundColor(Color("SportGold"))
                 
                 DatePicker("New Date", selection: $newDate)
                     .foregroundColor(.white)
