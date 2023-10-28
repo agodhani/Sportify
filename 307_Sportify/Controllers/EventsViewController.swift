4979da7e25058808798116ea4651e0083b07bf13
joshstseng <joshuastseng@gmail.com>
2023-10-27 23:14:07 -0400

    Create Event Button  and started Table View

--- 307_Sportify/Controllers/EventsViewController.swift
+++ 307_Sportify/Controllers/EventsViewController.swift
@@ -9,16 +9,21 @@ import Foundation
 import UIKit
 import SwiftUI
 
-class EventsViewController: UIViewController {
+class EventsViewController: UIViewController, UITableViewDataSource {
     
     @State var userAuth = UserAuthentication()
     @State var eventsm = EventMethods()
+    @IBOutlet weak var table: UITableView!
     
     // TODO JOSH
     // BUTTON - CREATE EVENT
     
     // TODO - in original Text(MY EVENTS) - line 55 in EventsView
     // I do .onAppear for allEvents - do this here
+    
+    class CustomTableViewCell: UITableViewCell {
+        // TODO
+    }
     
     private var myEventsText: UITextView = {
         let text = UITextView()
@@ -76,6 +81,19 @@ class EventsViewController: UIViewController {
         return text
     }()
     
+    private var createEventButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Create Event", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 18, weight: .bold)
+        button.backgroundColor = .sportGold
+        button.setTitleColor(.black, for: .normal)
+        button.layer.cornerRadius = 20
+        button.layer.masksToBounds = true
+        button.titleLabel?.lineBreakMode = .byWordWrapping
+        button.titleLabel?.textAlignment = .center
+        return button
+    }()
+    
     private let scrollView: UIScrollView = {
         let scrollView = UIScrollView()
         scrollView.clipsToBounds = true
@@ -85,10 +103,12 @@ class EventsViewController: UIViewController {
     
     override func viewDidLoad() {
         @State var currentUser = userAuth.currUser
+        //var currentUser = User(id: "1", name: "test", email: "testEmail", radius: 1, zipCode: "", sportsPreferences: [0], privateAccount: true, profilePicture: "1", age: 1, birthday: Date(), friendList: [], blockList: [], eventsAttending: [], eventsHosting: [], suggestions: [])
         super.viewDidLoad()
         view.backgroundColor = .black
         
         // Add subviews
+        view.addSubview(createEventButton)
         view.addSubview(myEventsText)
         view.addSubview(eventsText)
         view.addSubview(sportText)
@@ -100,35 +120,69 @@ class EventsViewController: UIViewController {
         
     }
     
+    // TODO
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    // TODO
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomTableViewCell
+        // set the stuff
+        return cell
+    }
+    
+    
+    
     // Organize view
     override func viewDidLayoutSubviews() {
+        
         super.viewDidLayoutSubviews()
         let size = view.width / 1.2
         view.frame = view.bounds
         
+        createEventButton.addTarget(self, action: #selector(tappedCreateEvent), for: .touchUpInside)
+        
+        createEventButton.frame = CGRect(x: 250,
+                                        y: 40,
+                                        width: 125,
+                                        height: 40)
+        
         myEventsText.frame = CGRect(x: (view.width - size) / 2,
-                                    y: 50,
+                                    y: 80, // was 50
                                     width: size,
                                     height: size)
+        
         eventsText.frame = CGRect(x: -450,
-                                  y: 120,
+                                  y: 150,
                                   width: 1000,
                                   height: 50)
+        
         sportText.frame = CGRect(x: -25,
-                                y: 120,
-                                width: size,
-                                height: 50)
-        locationText.frame = CGRect(x: 85,
-                                y: 120,
-                                width: size,
-                                height: 50)
-        dateText.frame = CGRect(x: 190,
-                                y: 120,
+                                y: 150,
                                 width: size,
                                 height: 50)
         
+        locationText.frame = CGRect(x: 85,
+                                y: 150,
+                                width: size,
+                                height: 50)
+        
+        dateText.frame = CGRect(x: 190,
+                                y: 150,
+                                width: size,
+                                height: 50)
         
     }
+    
+
+    
+    @objc private func tappedCreateEvent() { // TODO CreateEventViewController()
+        print("tappedCreateEvent")
+        //let vc = CreateEventViewController()
+        //navigationController?.pushViewController(vc, animated: true)
+    }
+    
 }
 
 #Preview {
