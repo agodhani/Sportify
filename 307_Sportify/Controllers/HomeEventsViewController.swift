e36a7ee276460a93678c0978d0bd6f4aed2c90a3
agodhani <agodhani2021@gmail.com>
2023-10-29 16:58:11 -0400

    picker filters added with filter functionality

--- 307_Sportify/Controllers/HomeEventsViewController.swift
+++ 307_Sportify/Controllers/HomeEventsViewController.swift
@@ -28,12 +28,19 @@ protocol AllEventsDelegate: AnyObject {
     func eventsDidUpdate()
 }
 
-class HomeEventsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, AllEventsDelegate, UISearchBarDelegate {
+class HomeEventsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, AllEventsDelegate, UISearchBarDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
+    
     @ObservedObject var allEvents = AllEvents()
     private var searching = false;
+    private var filterSports = false;
+    private var filterMaxParticipants = false;
+    var selectedSport: Int?
+    var selectedParticipants: Int?
+    let sportList = ["Tennis", "Table Tennis", "Volleyball", "Soccer", "Basketball", "Football", "Baseball", "Badminton", "Golf", "Cycling", "Running", "Hockey", "Spikeball", "Handball", "Lacrosse", "Squash", "None"]
+    let numberList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if(searching) {
+        if(searching || filterSports || filterMaxParticipants) {
             let eventz = allEvents.filteredEvents
             let counter = eventz.count
             return counter
@@ -44,10 +51,10 @@ class HomeEventsViewController: UIViewController, UITableViewDataSource, UITable
         }
         
     }
-    
+    //filter table views
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        if(searching) {
+        if(searching || filterSports || filterMaxParticipants) {
             let eventz = allEvents.filteredEvents
             cell.textLabel?.text = eventz[indexPath.row].name
             return cell
@@ -59,7 +66,7 @@ class HomeEventsViewController: UIViewController, UITableViewDataSource, UITable
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if searching {
+        if searching || filterSports || filterMaxParticipants {
             let selectedEvent = allEvents.filteredEvents[indexPath.row]
             let vc = SingleEventViewController()
             vc.event = selectedEvent
@@ -89,6 +96,7 @@ class HomeEventsViewController: UIViewController, UITableViewDataSource, UITable
         return text
     }()
     
+    //filtering location
     private let locationLabel: UILabel = {
         let label = UILabel()
         label.text = "Location"
@@ -109,7 +117,7 @@ class HomeEventsViewController: UIViewController, UITableViewDataSource, UITable
         }
     }
     
-    
+    //filtering event Host
     private let eventHostLabel: UILabel = {
         let label = UILabel()
         label.text = "Event Host"
@@ -130,6 +138,95 @@ class HomeEventsViewController: UIViewController, UITableViewDataSource, UITable
         }
     }
     
+    //pickers
+    // sport tag = 1
+    private var sportPicker: UIPickerView = {
+        let picker = UIPickerView()
+        picker.tag = 1
+        return picker
+    }()
+    
+    // number tag = 2
+    private var numberMaxParticipants: UIPickerView = {
+        let picker = UIPickerView()
+        picker.tag = 2
+        return picker
+    }()
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if (pickerView.tag == 1) {
+            return sportList.count
+        } else {
+            return numberList.count
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        if (pickerView.tag == 1) {
+            let item = sportList[row]
+            return item
+        } else {
+            let item = numberList[row]
+            let itemString = String(item)
+            return itemString
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+        if (pickerView.tag == 1) {
+            return NSAttributedString(string: sportList[row], attributes: [NSAttributedString.Key.foregroundColor: UIColor.sportGold])
+        } else {
+            return NSAttributedString(string: String(numberList[row]), attributes: [NSAttributedString.Key.foregroundColor: UIColor.sportGold])
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if pickerView.tag == 1 { // Sport Picker
+            selectedSport = row
+            if(row == 16) {
+                filterSports = false
+                eventsDidUpdate()
+            } else {
+                filterSports = true
+                if(searching) {
+                    allEvents.filteredEvents = allEvents.filteredEvents.filter({event in
+                        event.sport == row})
+                    eventsDidUpdate()
+                } else {
+                    allEvents.filteredEvents = allEvents.events.filter({event in
+                        event.sport == row})
+                    eventsDidUpdate()
+                }
+            }
+            
+        } else if pickerView.tag == 2 { // Number Picker
+            selectedParticipants = numberList[row]
+            if(row == 0) {
+                filterMaxParticipants = false
+                eventsDidUpdate()
+            } else {
+                filterMaxParticipants = true
+                if(searching) {
+                    allEvents.filteredEvents = allEvents.filteredEvents.filter({event in
+                        event.maxParticipants == row})
+                    eventsDidUpdate()
+                } else {
+                    allEvents.filteredEvents = allEvents.events.filter({event in
+                        event.maxParticipants == row})
+                    eventsDidUpdate()
+                }
+            }
+        }
+    }
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    
+    //searchBar filtering
     private var searchBar: UISearchBar = {
         let searchBar = UISearchBar()
         searchBar.placeholder = "Event Name"
@@ -144,19 +241,59 @@ class HomeEventsViewController: UIViewController, UITableViewDataSource, UITable
             searching = true
         }
         if(filterLocationSlider.isOn) {
-            allEvents.filteredEvents = allEvents.events.filter({event in event.location.lowercased()
-                .contains(searchText.lowercased())})
-            eventsDidUpdate()
+            if(filterSports || filterMaxParticipants) {
+                allEvents.filteredEvents = allEvents.filteredEvents.filter({event in event.location.lowercased()
+                    .contains(searchText.lowercased())})
+                eventsDidUpdate()
+            } else {
+                allEvents.filteredEvents = allEvents.events.filter({event in event.location.lowercased()
+                    .contains(searchText.lowercased())})
+                eventsDidUpdate()
+            }
         } else if(filterHostSlider.isOn) {
-            allEvents.filteredEvents = allEvents.events.filter({event in event.eventHost.lowercased()
-                .contains(searchText.lowercased())})
-            eventsDidUpdate()
+            if(filterSports || filterMaxParticipants) {
+                allEvents.filteredEvents = allEvents.filteredEvents.filter({event in event.eventHost.lowercased()
+                    .contains(searchText.lowercased())})
+                eventsDidUpdate()
+            } else {
+                allEvents.filteredEvents = allEvents.events.filter({event in event.eventHost.lowercased()
+                    .contains(searchText.lowercased())})
+                eventsDidUpdate()
+            }
         } else {
-            allEvents.filteredEvents = allEvents.events.filter({event in event.name.lowercased()
-                .contains(searchText.lowercased())})
-            eventsDidUpdate()
+            if(filterSports || filterMaxParticipants) {
+                allEvents.filteredEvents = allEvents.filteredEvents.filter({event in event.name.lowercased()
+                    .contains(searchText.lowercased())})
+                eventsDidUpdate()
+            } else {
+                allEvents.filteredEvents = allEvents.events.filter({event in event.name.lowercased()
+                    .contains(searchText.lowercased())})
+                eventsDidUpdate()
+            }
         }
     }
+    
+    private var sportText: UITextView = {
+        let text = UITextView()
+        text.text = "Sport"
+        text.textColor = .sportGold
+        text.backgroundColor = .clear
+        text.textAlignment = .center
+        text.font = .systemFont(ofSize: 15, weight: .regular)
+        text.isEditable = false
+        return text
+    }()
+    
+    private var maxParticipantsText: UITextView = {
+        let text = UITextView()
+        text.text = "Max Participants"
+        text.textColor = .sportGold
+        text.backgroundColor = .clear
+        text.textAlignment = .center
+        text.font = .systemFont(ofSize: 15, weight: .regular)
+        text.isEditable = false
+        return text
+    }()
     
     private let table: UITableView = {
         let table = UITableView()
@@ -180,6 +317,23 @@ class HomeEventsViewController: UIViewController, UITableViewDataSource, UITable
         view.addSubview(eventHostLabel)
         view.addSubview(filterHostSlider)
         view.addSubview(filterLocationSlider)
+        
+        view.addSubview(sportText)
+        sportPicker.tag = 1
+        sportPicker.delegate = self
+        sportPicker.dataSource = self
+        view.addSubview(sportPicker)
+        sportPicker.center = self.view.center
+        sportPicker.selectRow(16, inComponent: 0, animated: false)
+        
+        numberMaxParticipants.tag = 2
+        numberMaxParticipants.delegate = self
+        numberMaxParticipants.dataSource = self
+        numberMaxParticipants.center = self.view.center
+        view.addSubview(numberMaxParticipants)
+        view.addSubview(maxParticipantsText)
+
+        
         filterLocationSlider.addTarget(self, action: #selector(switchLocationSliderChanged), for: .valueChanged)
         filterHostSlider.addTarget(self, action: #selector(switchHostSliderChanged), for: .valueChanged)
         table.dataSource = self
@@ -197,30 +351,57 @@ class HomeEventsViewController: UIViewController, UITableViewDataSource, UITable
         view.frame = view.bounds
         view.backgroundColor = .black
         myEventsText.frame = CGRect(x: -50,
-                                    y: 50,
+                                    y: 0,
                                     width: size,
                                     height: size)
-        searchBar.frame = CGRect(x:0, y: 100, width: view.bounds.size.width, height: 44)
+        searchBar.frame = CGRect(x:0, y: 50, width: view.bounds.size.width, height: 44)
         table.frame = CGRect(x: (view.width - size) / 2,
                              y: 250,
                              width: size,
                              height: size * 1.3)
         locationLabel.frame = CGRect(x: 50,
-                                    y: 145,
+                                    y: 95,
                                     width: size,
                                     height: 50)
         filterLocationSlider.frame = CGRect(x:298,
-                                       y: 150,
+                                       y: 100,
                                        width: 1,
                                        height: 1)
         eventHostLabel.frame = CGRect(x: 50,
-                                    y: 185,
+                                    y: 135,
                                     width: size,
                                     height: 50)
         filterHostSlider.frame = CGRect(x:298,
-                                       y: 190,
+                                       y: 140,
                                        width: 1,
                                        height: 1)
+        sportText.frame = CGRect(x: -15,
+                                  y: 170,
+                                  width: 180,
+                                  height: 50)
+        
+        sportPicker.frame = CGRect(x: 50,
+                                    y: 190,
+                                    width: 180,
+                                    height: 50)
+        sportText.frame = CGRect(x: -15,
+                                  y: 170,
+                                  width: 100,
+                                  height: 50)
+        
+        sportPicker.frame = CGRect(x: -15,
+                                    y: 180,
+                                    width: 150,
+                                    height: 50)
+        maxParticipantsText.frame = CGRect(x: 125,
+                                  y: 170,
+                                  width: 180,
+                                  height: 50)
+        
+        numberMaxParticipants.frame = CGRect(x: 175,
+                                    y: 190,
+                                    width: 180,
+                                    height: 50)
     }
 }
 
