abc17fcf158ecc083bcf605eef97dbc9841bce81
Alexcmoraes <alexandre.c.moraes@hotmail.com>
2023-11-01 11:59:28 -0400

    Upload profile pic to db

--- 307_Sportify/Controllers/StorageManager.swift
+++ 307_Sportify/Controllers/StorageManager.swift
@@ -14,26 +14,33 @@ final class StorageManager {
     
     private let storage = Storage.storage().reference()
     
-    public typealias uploadPicCompletion = (Result<String, Error>)
+    public typealias uploadPicCompletion = (Result<String, Error>) -> Void
     
-    public func uploadProfilePic(with data: Data, fileName: String, completion: uploadPicCompletion) {
-        storage.child("profilePictures/\(fileName)").putData(data, completion: { metadata, error in
+    public func uploadProfilePic(with data: Data, fileName: String, completion: @escaping uploadPicCompletion) {
+        storage.child("profilePictures/\(fileName)").putData(data, metadata: nil, completion: { metadata, error in
             guard error == nil else {
                 // Failed
                 print("failed to upload profile pic")
+                completion(.failure(StorageErrors.failedToUpload))
                 return
             }
             
-//            self.storage.child("profilePictures/\(fileName)").downloadURL(completion: { url, error Int
-//                guard let url = url else {
-//                    print("failed to get url")
-//                    return
-//                }
-//            })
+            self.storage.child("profilePictures/\(fileName)").downloadURL(completion: { url, error in
+                guard let url = url else {
+                    print("failed to get url")
+                    completion(.failure(StorageErrors.filedToGetDownloadUrl))
+                    return
+                }
+                
+                let urlString = url.absoluteString
+                print("url returned: \(urlString)")
+                completion(.success(urlString))
+            })
         })
     }
     
     public enum StorageErrors: Error {
         case failedToUpload
+        case filedToGetDownloadUrl
     }
 }
