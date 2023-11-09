4100bf3401228b646cd7f88cae38b97cfcf0bb75
joshstseng <joshuastseng@gmail.com>
2023-11-09 12:12:45 -0500

    Create AddFriendsViewController

---
+++ 307_Sportify/Controllers/Profile/AddFriendsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AddFriendsViewController.swift
+//  307_Sportify
+//
+//  Created by Joshua Tseng on 11/9/23.
+//
+
+import UIKit
+import SwiftUI
+import Firebase
+
+class AddFriendsViewController: UIViewController {
+    
+    var userAuth = UserAuthentication()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+    }
+    
+}
