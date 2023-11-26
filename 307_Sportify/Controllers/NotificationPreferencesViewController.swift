6836ccc1f4c4e8e345dd81c72dcebc8a76150a0e
Alexcmoraes <alexandre.c.moraes@hotmail.com>
2023-11-26 16:58:41 -0500

    Finished notification preferences view

--- 307_Sportify/Controllers/NotificationPreferencesViewController.swift
+++ 307_Sportify/Controllers/NotificationPreferencesViewController.swift
@@ -9,13 +9,20 @@ import UIKit
 
 class NotificationPreferencesViewController: UIViewController {
 
+    private let logoView: UIImageView = {
+        let logoView = UIImageView()
+        logoView.image = UIImage(named: "SportifyLogoOriginal")
+        logoView.contentMode = .scaleAspectFit
+        return logoView
+    }()
+    
     private let notificationsText: UITextView = {
         let text = UITextView()
         text.text = "Notifications"
         text.backgroundColor = .black
         text.textColor = .white
         text.textAlignment = .center
-        text.font = .systemFont(ofSize: 20, weight: .semibold)
+        text.font = .systemFont(ofSize: 30, weight: .semibold)
         return text
     }()
     
@@ -23,32 +30,68 @@ class NotificationPreferencesViewController: UIViewController {
         let text = UITextView()
         text.text = "General Notifications"
         text.textColor = .white
+        text.backgroundColor = .black
         text.font = .systemFont(ofSize: 20, weight: .regular)
         return text
+    }()
+    
+    private let generalNotificationsSlider: UISwitch = {
+        let toggle = UISwitch()
+        return toggle
     }()
     
     private let dmNotificationsText: UITextView = {
         let text = UITextView()
         text.text = "DM Notifications"
         text.textColor = .white
+        text.backgroundColor = .black
         text.font = .systemFont(ofSize: 20, weight: .regular)
         return text
+    }()
+    
+    private let dmNotificationsSlider: UISwitch = {
+        let toggle = UISwitch()
+        return toggle
     }()
     
     private let eventNotificationsText: UITextView = {
         let text = UITextView()
         text.text = "Event Notifications"
         text.textColor = .white
+        text.backgroundColor = .black
         text.font = .systemFont(ofSize: 20, weight: .regular)
         return text
+    }()
+    
+    private let eventNotificationsSlider: UISwitch = {
+        let toggle = UISwitch()
+        return toggle
     }()
     
     private let friendRequestNotificationsText: UITextView = {
         let text = UITextView()
         text.text = "Friend Request Notifications"
         text.textColor = .white
+        text.backgroundColor = .black
         text.font = .systemFont(ofSize: 20, weight: .regular)
         return text
+    }()
+    
+    private let friendRequestNotificationsSlider: UISwitch = {
+        let toggle = UISwitch()
+        return toggle
+    }()
+    
+    private let updatePreferencesButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Update Preferences", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 18, weight: .bold)
+        button.backgroundColor = .sportGold
+        button.setTitleColor(.black, for: .normal)
+        button.layer.cornerRadius = 25
+        button.layer.masksToBounds = true
+        button.titleLabel?.textAlignment = .center
+        return button
     }()
     
     override func viewDidLoad() {
@@ -56,38 +99,69 @@ class NotificationPreferencesViewController: UIViewController {
         view.backgroundColor = .black
         
         // Add subviews
+        view.addSubview(logoView)
         view.addSubview(notificationsText)
         view.addSubview(generalNotificationsText)
+        view.addSubview(generalNotificationsSlider)
         view.addSubview(dmNotificationsText)
+        view.addSubview(dmNotificationsSlider)
         view.addSubview(eventNotificationsText)
+        view.addSubview(eventNotificationsSlider)
         view.addSubview(friendRequestNotificationsText)
+        view.addSubview(friendRequestNotificationsSlider)
+        view.addSubview(updatePreferencesButton)
     }
     
     // Organize view
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        let size = view.width / 1.2
         
-        notificationsText.frame = CGRect(x: 134,
-                                         y: 60,
-                                         width: 135,
+        logoView.frame = CGRect(x: (view.width - size) / 2,
+                                y: -10,
+                                width: size,
+                                height: size)
+        notificationsText.frame = CGRect(x: 5,
+                                         y: logoView.bottom - 50,
+                                         width: view.width,
                                          height: 40)
         
-        generalNotificationsText.frame = CGRect(x: 45,
-                                                y: notificationsText.bottom + 40,
+        generalNotificationsText.frame = CGRect(x: 25,
+                                                y: notificationsText.bottom + 50,
                                                 width: view.width,
-                                                height: 25)
-        dmNotificationsText.frame = CGRect(x: 45,
+                                                height: 30)
+        generalNotificationsSlider.frame = CGRect(x: 315,
+                                                  y: notificationsText.bottom + 55,
+                                                  width: view.width,
+                                                  height: 20)
+        dmNotificationsText.frame = CGRect(x: 25,
                                                 y: generalNotificationsText.bottom + 40,
                                                 width: view.width,
-                                                height: 25)
-        eventNotificationsText.frame = CGRect(x: 45,
+                                                height: 30)
+        dmNotificationsSlider.frame = CGRect(x: 315,
+                                                  y: generalNotificationsText.bottom + 45,
+                                                  width: view.width,
+                                                  height: 20)
+        eventNotificationsText.frame = CGRect(x: 25,
                                                 y: dmNotificationsText.bottom + 40,
                                                 width: view.width,
-                                                height: 25)
-        friendRequestNotificationsText.frame = CGRect(x: 45,
+                                                height: 30)
+        eventNotificationsSlider.frame = CGRect(x: 315,
+                                                  y: dmNotificationsText.bottom + 45,
+                                                  width: view.width,
+                                                  height: 20)
+        friendRequestNotificationsText.frame = CGRect(x: 25,
                                                 y: eventNotificationsText.bottom + 40,
                                                 width: view.width,
-                                                height: 25)
+                                                height: 30)
+        friendRequestNotificationsSlider.frame = CGRect(x: 315,
+                                                  y: eventNotificationsText.bottom + 45,
+                                                  width: view.width,
+                                                  height: 20)
+        updatePreferencesButton.frame = CGRect(x: 90,
+                                    y: friendRequestNotificationsText.bottom + 70,
+                                    width: 225,
+                                    height: 50)
     }
 }
 
